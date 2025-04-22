41cfd7d6d27a4a88487111402d9abbaec1eafbc0
maprevidelli <marco.previdelli@gmail.com>
2025-04-22 20:30:19 -0300

    2nd commit

---
+++ 2.1_variaveis.py
@@ -0,0 +1,6 @@
+#2.1
+message = 'Happy Easter 2025'
+print(message)
+# 2.2
+message = 'Happy New Year, mate'
+print(message)
---
+++ 2.3_variaveis.py
@@ -0,0 +1,14 @@
+# 2.3
+nome_user = input('Hello, what is your name?: ')
+print(f'Olá {nome_user}, gostaria de aprender um pouco de Python hoje?')
+
+# 2.4
+nome_maiusculo = nome_user.capitalize()
+nome_minusculo = nome_user.lower()
+print(nome_minusculo)
+print(nome_maiusculo)
+
+# 2.5
+nome = "Albert Einstein"
+citacao = "Uma pessoa que nunca cometeu um erro nunca tentou nada de novo"
+print(f'{nome} disse uma vez: "{citacao}"')
