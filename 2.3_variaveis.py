effce7c5de5b4479b48df92839d5ad3a12770d8f
maprevidelli <marco.previdelli@gmail.com>
2025-04-23 20:39:58 -0300

    atualização

--- 2.3_variaveis.py
+++ 2.3_variaveis.py
@@ -8,7 +8,20 @@ nome_minusculo = nome_user.lower()
 print(nome_minusculo)
 print(nome_maiusculo)
 
-# 2.5
-nome = "Albert Einstein"
-citacao = "Uma pessoa que nunca cometeu um erro nunca tentou nada de novo"
-print(f'{nome} disse uma vez: "{citacao}"')
+# 2.6
+famous_person = "Albert Einstein"
+message = "Uma pessoa que nunca cometeu um erro nunca tentou nada de novo"
+print(f'{famous_person} disse uma vez: "{message}"')
+
+# 2.7
+name = '   Darth Vader '
+print(f'\no nome em destaque é:{name}\n \tquer voce queria ou não!')
+print('='* 45)
+print(f'Remover espaço na esquerda: {name.lstrip()}')
+print(f'Remover espaço na direita: {name.rstrip()}')
+print(f'ambos lados: {name.strip()}')
+
+# 2.8
+filename = 'python_notes.txt'
+print(f'Remove Sulfixo: {filename.removesuffix(".txt")}')
+print(f'Remove prefixo: {filename.removeprefix("python_")}')
