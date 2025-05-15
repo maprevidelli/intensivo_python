87044428cb4056ec85d287240b8d7ed274616531
maprevidelli <marco.previdelli@gmail.com>
2025-05-15 20:37:46 -0300

    update

--- 3.9_listas.py
+++ 3.9_listas.py
@@ -1,3 +1,15 @@
 # 3.9
 cidades = ['Hong Kong', 'Manchester', 'Roma', 'Napoli', 'Amsterdan']
-print(len(cidades))
+opcao = [0, 1, 2, 3, 4]
+term = True
+while term:
+    try:
+        escolha = int(input('Entre com a opção: '))
+        if escolha not in opcao:
+            print('Fora da Range')
+            continue
+    except ValueError:
+        print('opção inválida!') 
+        continue 
+    print(f'Cidade escolhida foi:{cidades[escolha]}')
+    term = False
