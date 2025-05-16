e4fb67e3be94b5489f5445a8a45e268ecb9f8536
maprevidelli <marco.previdelli@gmail.com>
2025-05-16 19:41:42 -0300

    mais atividades

---
+++ 4.1_listas.py
@@ -0,0 +1,5 @@
+# 4.1
+pizzas = ['Atum', 'Calabresa', 'Quatro-Queijos']
+for pizza in pizzas:
+    print(f'Gosto de pizza de {pizza}!')
+print('Eu amo Pizza!!')
---
+++ 4.2_listas.py
@@ -0,0 +1,9 @@
+# 4.2
+animais = ['Leão', 'Tigre', 'Onça']
+for animal in animais:
+    if animal == 'Onça':
+        print(f'{animal} é muito linda, no mato!')
+    else:
+        print(f'{animal} é um formidável gato do mato')
+print('Adoraveis felinos!')
+
